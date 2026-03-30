bfea3122061ab4eb92a49818ecb913771f3a558c
ThanhBinh0904 <nguyenaithanhbinh@gmail.com>
2026-03-30 14:55:48 +0700

    feat: Create CarSearch component and styling with CSS Modules

--- frontend/my-app/src/page/SearchPage.jsx
+++ frontend/my-app/src/page/SearchPage.jsx
@@ -1,8 +1,15 @@
+import CarSearch from "../components/CarSearch";
+
 const SearchPage = () => {
+  const handleSearch = (values) => {
+    console.log("Giá trị tìm kiếm:", values);
+  };
+
   return (
     <div>
       <h1>Search Page</h1>
       <p>Kết quả tìm kiếm phòng.</p>
+      <CarSearch onSearch={handleSearch} />
     </div>
   );
 };
